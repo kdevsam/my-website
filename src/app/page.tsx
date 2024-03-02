bc42fe7c4ccefce1c4804105b4bf281d8026c187
Samer Kozrosh <kdevsam@gmail.com>
2024-03-01 22:26:31 -0500

    Changed to nextJS

---
+++ src/app/page.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { FloatingNav } from "@/app/components/ui/floating-navbar";
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+import { WavyBackground } from "./components/ui/wavy-background";
+
+export default function FloatingNavDemo() {
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+      icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "About",
+      link: "/about",
+      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+      icon: (
+        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    },
+  ];
+  return (
+    <div className="relative  w-full">
+      <FloatingNav navItems={navItems} />
+      <WavyBackground className="max-w-4xl mx-auto pb-40">
+        <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+          Samer Kozrosh
+        </p>
+        <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
+          Leverage the power of canvas to create a beautiful hero section
+        </p>
+      </WavyBackground>
+    </div>
+  );
+}
